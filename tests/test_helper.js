cb4676a10af41ac53ee037446f40ed5286a133a7
Malin Häggblom <malin.haggblom@centria.fi>
2022-06-09 15:51:24 +0300

    User added

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   { _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0 },
@@ -14,6 +15,14 @@ const blogsInDb = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const initialUsers = [
+    { _id: '6274d665d304c0d5d490b1c0', username:'root', name:'Superuser', passwordHash:'$2b$10$0nzRHT2r7YevIedLGY7S0OHr07fS8ge9x8xNYQxXDs8k2w7RS0Tta' }
+  ]
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(u => u.toJSON())
+}
+
 module.exports = {
-  initialBlogs, blogsInDb
+  initialBlogs, blogsInDb, initialUsers, usersInDb
 }
